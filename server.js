7120aea38e83aaf652481968c9be6459a263069b
redlean <contact@redlean.io>
2018-07-07 17:24:30 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'dist/')));
 // in the dist directory
 
 app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
  });
 // Start the app by listening on the default
 // Heroku port
